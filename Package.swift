e6b947016353ddfc04b1d568e503ef27a9951055
Georgie Ivanov <georgie@barimax.bg>
2025-12-09 11:48:46 +0200

    Initial commit. Create library

--- Package.swift
+++ Package.swift
@@ -1,22 +1,37 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "cs-webauthn",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "cs-webauthn",
+            name: "CSWebAuthn",
             targets: ["cs-webauthn"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/swift-server/swift-webauthn.git", from: "1.0.0-alpha.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "cs-webauthn"
+            name: "cs-webauthn",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "WebAuthn", package: "swift-webauthn")
+            ],
         ),
         .testTarget(
             name: "cs-webauthnTests",
